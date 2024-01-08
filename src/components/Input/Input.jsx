024ac484cb16dddd8f2e23001ae060f8575b887b
Dushmanthr <dushmantha15ranathunga@gmail.com>
2024-01-09 01:35:09 +0530

    complete design

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './input.css'
+import Img from '../../assets/Img/img.png'
+import Attach from '../../assets/Img/attach.png'
 
 const Input = () => {
   return (
     <div className="input">
-
+        <input type="text" placeholder='Type Something...'/>
+        <div className="send">
+          <img src={Attach} alt="" />
+          <input type="file" style={{display:"none"}} id="file"/>
+          <label htmlFor="file">
+            <img src={Img} alt="" />
+            
+          </label>
+          <button>Send</button>
+        </div>
     </div>
-  )
+  ) 
 }
 
 export default Input
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './message.css'
 
 const Message = () => {
   return (
-    <div>
-      Message
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/1055691/pexels-photo-1055691.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <span>just now</span>
+        
+      </div>
+      <div className="messageContent">
+            <p>hello</p>
+          <img src="https://images.pexels.com/photos/1055691/pexels-photo-1055691.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        </div>
     </div>
   )
 }
